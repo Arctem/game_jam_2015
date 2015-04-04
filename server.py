9e21d63da4da67c6fc2709da60d79610cbbbc88a
James Alexander <decjimmy@aol.com>
2015-04-04 14:47:12 -0600

    fixing errors in server.py

--- server.py
+++ server.py
@@ -85,29 +85,29 @@ def create_world():
     world.add_room(barracks)
     world.add_room(canteen)
 
-    #Room Additions
-    #name;keyword:keyword:...:keyword;attribute:attribute;short_desc;description;possible_start
-	f = open('list_rooms.txt', 'r')
+    #Room Additions #name;keyword:keyword:...:keyword;attribute:attribute;short_desc;description;possible_start
+    f = open('list_rooms.txt', 'r')
     rooms = f.read()
-    rooms = rooms.split()
+    rooms = rooms.split('\n')
     for i in rooms:
-        if i[0] != '#':
-            name, keyword, attribute, short_desc, description, possible_start = i.split(';');
-            print (name)
-            keyword = keyword.split(':')
+        if i and i[0] != '#':
+            """name, keyword, attribute, short_desc, description, possible_start = i.split(';')"""
+            print (i)
+            """keyword = keyword.split(':')
             attribute = attribute.split(':')
             attribute = list(map(lambda a: a.split(',', 1), attribute))
             if possible_start == 0:
                     possible_start = False
             else:
                     possible_start = True
-            world.add_room(Room(name, short_desc, description,keyword,attribute, possible_start))
+            world.add_room(Room(name, short_desc, description,keyword,attribute, possible_start))"""
     f.close()
-	
+ 
+    """
     #Decoration Additions
     #name;keyword:keyword:...:keyword;attribute:attribute;short_desc;description;room
 
-	f = open('list_decorations.txt', 'r')
+    f = open('list_decorations.txt', 'r')
     decs = f.read()
     decs = decs.split()
     for i in decs:
@@ -124,8 +124,7 @@ def create_world():
     f.close()
 
     #Item Additions
-
-	f = open('list_items.txt', 'r')
+    f = open('list_items.txt', 'r')
     decs = f.read()
     decs = decs.split()
     for i in decs:
@@ -143,7 +142,7 @@ def create_world():
     
     #Connections Additions
 
-	f = open('list_connections.txt', 'r')
+    f = open('list_connections.txt', 'r')
     decs = f.read()
     decs = decs.split()
     for i in decs:
@@ -163,7 +162,7 @@ def create_world():
                         if destination in d.keywords:
                             s.add_connection(connection(s,d, short_desc, description, pass_desc, keyword,attribute, locked))
                             break
-    f.close()
+    f.close()"""
     
     return world
 
