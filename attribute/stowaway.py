d99b28ab0d20d6d8368dae9aa0d6b12b06e33ad9
James Alexander <decjimmy@aol.com>
2015-04-05 00:02:14 -0600

    stowaway stuff

--- attribute/stowaway.py
+++ attribute/stowaway.py
@@ -18,4 +18,4 @@ class Stowaway(Person):
             targets = list(filter(lambda c: isinstance(c, Decoration) and 
                 c.name.lower() == args.lower(), player.room.contents))
             if len(targets) == 1:
-                player.send_msg('The stowaway spits in your face')
+                player.send_msg('"Please don\'t hurt me," the hostage begged."I want to help you," you say, still unsure if you want to help him or dispose of him. You were ordered to get rid of him as he is an evil man, but you also are wary of elimination life.')
