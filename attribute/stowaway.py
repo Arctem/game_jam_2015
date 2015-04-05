7d716c2beddd4f678b60ce481ec69c5c612759bf
Russell White <rarctem@gmail.com>
2015-04-05 10:52:36 -0600

    Some whitespace cleaning.

--- attribute/stowaway.py
+++ attribute/stowaway.py
@@ -6,19 +6,19 @@ from decoration import Decoration
 class Person(Attribute):
     def __init__(self):
         Attribute.__init__(self)
-        
+
 class Stowaway(Person, Shootable):
     def __init__(self, args):
         Person.__init__(self)
         Shootable.__init__(self, parent)
 
         self.commands['INTERROGATE'] = self.interrogate
-        
+
     def interrogate(self, player, args):
         if len(args) == 0:
             player.send_msg('Who do you want to interrogate?')
         else:
-            targets = list(filter(lambda c: isinstance(c, Decoration) and 
+            targets = list(filter(lambda c: isinstance(c, Decoration) and
                 c.name.lower() == args.lower(), player.room.contents))
             if len(targets) == 1:
                 player.send_msg('"Please don\'t hurt me," the hostage begged."I want to help you," you say, still unsure if you want to HELP him or dispose of him. You were ordered to get ELIMINATE him as he is an evil man, but you also are wary of elimination life.')
