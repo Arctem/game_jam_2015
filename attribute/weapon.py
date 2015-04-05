b03d21eda33e235d4d7fdbe0977721d560e3dcbe
Russell White <rarctem@gmail.com>
2015-04-05 10:51:28 -0600

    Create Shootable attribute.

--- attribute/weapon.py
+++ attribute/weapon.py
@@ -22,8 +22,31 @@ class RangedWeapon(Weapon):
                 targets[0].send_msg('{} shoots you!'.format(player.name))
                 targets[0].kill('shot')
 
+    def valid_target(item, args):
+        if isinstance(item, Player) and c.name.lower() == args.lower():
+            return True
+        elif:
+            for attr in item.attributes:
+                if isinstance(attr, Shootable) and args.lower in item.keywords:
+                    return True
+        else:
+            return False
+
+
 
 
 class MeleeWeapon(Weapon):
     def __init__(self, args):
         Weapon.__init__(self)
+
+
+
+class Shootable(Attribute):
+    def __init__(self):
+        Attribute.__init__(self)
+        self.shot = False
+
+    def shoot(self, shooter, weapon):
+        self.shot = True
+        shooter.inform_others('{} shot {}.'.format(shooter.name,
+            self.parent.name))
